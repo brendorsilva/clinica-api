de04f3e90fd247d6149c9e9c32edb14d35c68ed3
brendorsilva <brendorocha@outlook.com>
2026-02-05 17:42:35 -0300

    fix: correção para aceitar conexão via TiDB

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -14,6 +14,9 @@ export class PrismaService
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      ssl: {
+        rejectUnauthorized: true,
+      },
     });
 
     super({ adapter });
